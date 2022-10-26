8fc2590ce36d8640ed6f91f70bfafbc289b50bf8
Michael Bumann <hello@michaelbumann.com>
2022-10-26 18:29:28 +0200

    Simple proxy for LN addresses
    This proxy app loads the lnurlp and keysend details from a lightning address and returns the JSON

---
+++ go.mod
@@ -0,0 +1,24 @@
+module ln-address-cors-proxy
+
+go 1.17
+
+require (
+	github.com/getsentry/sentry-go v0.14.0
+	github.com/joho/godotenv v1.4.0
+	github.com/kelseyhightower/envconfig v1.4.0
+	github.com/labstack/echo/v4 v4.9.0
+)
+
+require (
+	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
+	github.com/labstack/gommon v0.3.1 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.16 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasttemplate v1.2.1 // indirect
+	golang.org/x/crypto v0.0.0-20220926161630-eccd6366d1be // indirect
+	golang.org/x/net v0.0.0-20221002022538-bcab6841153b // indirect
+	golang.org/x/sys v0.0.0-20220928140112-f11e5e49a4ec // indirect
+	golang.org/x/text v0.3.7 // indirect
+	golang.org/x/time v0.0.0-20220922220347-f3bd1da661af // indirect
+)
---
+++ main.go
@@ -0,0 +1,133 @@
+package main
+
+import (
+  "os"
+  "os/signal"
+  "context"
+  "fmt"
+  "github.com/labstack/echo/v4"
+  "github.com/labstack/echo/v4/middleware"
+  "log"
+  "time"
+  "net/http"
+  "strings"
+  "encoding/json"
+  "github.com/getsentry/sentry-go"
+  sentryecho "github.com/getsentry/sentry-go/echo"
+  "github.com/joho/godotenv"
+  "github.com/kelseyhightower/envconfig"
+)
+
+type Config struct {
+  SentryDSN             string `envconfig:"SENTRY_DSN"`
+  LogFilePath           string `envconfig:"LOG_FILE_PATH"`
+  Port                  int    `envconfig:"PORT" default:"3000"`
+}
+
+type LNResponse struct {
+    Lnurlp interface{} `json:"lnurlp"`
+    Keysend interface{} `json:"keysend"`
+}
+
+func main() {
+  c := &Config{}
+
+  // Load configruation from environment variables
+  err := godotenv.Load(".env")
+  if err != nil {
+    fmt.Println("Failed to load .env file")
+  }
+  err = envconfig.Process("", c)
+  if err != nil {
+    log.Fatalf("Error loading environment variables: %v", err)
+  }
+
+  e := echo.New()
+  e.HideBanner = true
+  e.Use(middleware.Logger())
+  e.Use(middleware.Recover())
+  e.Use(middleware.RequestID())
+  // Setup exception tracking with Sentry if configured
+  if c.SentryDSN != "" {
+    if err = sentry.Init(sentry.ClientOptions{
+      Dsn:          c.SentryDSN,
+      IgnoreErrors: []string{"401"},
+    }); err != nil {
+      log.Printf("sentry init error: %v", err)
+    }
+    defer sentry.Flush(2 * time.Second)
+    e.Use(sentryecho.New(sentryecho.Options{}))
+  }
+
+  e.GET("/lightning-address-details", func(c echo.Context) error {
+    responseBody := &LNResponse{}
+
+    ln := c.QueryParam("ln")
+    parts := strings.Split(ln, "@")
+    if len(parts) != 2 {
+      return c.JSON(http.StatusBadRequest, &responseBody)
+    }
+
+    keysendUrl := fmt.Sprintf("https://%s/.well-known/keysend/%s", parts[1], parts[0])
+    lnurlpUrl := fmt.Sprintf("https://%s/.well-known/lnurlp/%s", parts[1], parts[0])
+
+    keysendResponse, err := http.Get(keysendUrl)
+    if err != nil || keysendResponse.StatusCode > 300 {
+      e.Logger.Printf("No keysend details: %s - %v", ln, err)
+    } else {
+      defer keysendResponse.Body.Close()
+      var keysend interface{}
+      err = json.NewDecoder(keysendResponse.Body).Decode(&keysend)
+      if err != nil {
+        e.Logger.Printf("Invalid keysend JSON: %v", err)
+      } else {
+        responseBody.Keysend = keysend
+      }
+    }
+
+
+    lnurlpResponse, err := http.Get(lnurlpUrl)
+    if err != nil || lnurlpResponse.StatusCode > 300  {
+      e.Logger.Printf("No lnurlp details: %s - %v", ln, err)
+    } else {
+      defer lnurlpResponse.Body.Close()
+      var lnurlp interface{}
+      err = json.NewDecoder(lnurlpResponse.Body).Decode(&lnurlp)
+      if err != nil {
+        e.Logger.Printf("Invalid lnurlp JSON: %v", err)
+      } else {
+        responseBody.Lnurlp = lnurlp
+      }
+    }
+
+    // if both requests resulted in errors return a bad request. something must be wrong with the ln address
+    if lnurlpResponse == nil && keysendResponse == nil {
+      return c.JSON(http.StatusBadRequest, &responseBody)
+    }
+    // if both response have no success
+    if lnurlpResponse != nil && keysendResponse != nil && lnurlpResponse.StatusCode > 300 && keysendResponse.StatusCode > 300 {
+      return c.JSONPretty(lnurlpResponse.StatusCode, &responseBody, "  ")
+    }
+
+    // default return response
+    return c.JSONPretty(http.StatusOK, &responseBody, "  ")
+  })
+
+  // Start server
+  go func() {
+    if err := e.Start(fmt.Sprintf(":%v", c.Port)); err != nil && err != http.ErrServerClosed {
+      e.Logger.Fatal("shutting down the server")
+    }
+  }()
+  // Wait for interrupt signal to gracefully shutdown the server with a timeout of 10 seconds.
+  // Use a buffered channel to avoid missing signals as recommended for signal.Notify
+  quit := make(chan os.Signal, 1)
+  signal.Notify(quit, os.Interrupt)
+  <-quit
+  ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+  defer cancel()
+  if err := e.Shutdown(ctx); err != nil {
+    e.Logger.Fatal(err)
+  }
+
+}
